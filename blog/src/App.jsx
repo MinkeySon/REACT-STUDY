4b9cc2d3a7f00ffb534af0a95a4d01417e86c51e
HandMK <minkeygod@kangnam.ac.kr>
2025-03-12 23:33:13 +0900

    ✨ props 사용

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -7,9 +7,12 @@ function App() {
   let post = "강남 우동 맛집";
   let logo = 'React Logo';
 
-  let [t, setT] = useState(['여자 코트 추천','강남 우동 맛집','파이썬 독학']);
+  let [t, setT] = useState(['남자 코트 추천','강남 우동 맛집','파이썬 독학']);
   let [heart, setHeart] = useState(Array(t.length).fill(0)); // t개수 만큼 array 생김
   let [modal, setModal] = useState(false);
+  let [title, setTitle] = useState(0);
+  let [input, setInput] = useSate('');
+
   return (
     <div>
       <div className = "header">
@@ -27,8 +30,8 @@ function App() {
         t.map(function(a, i) {
           return (
             <div className='list' key={i}>
-              <h4 onClick={()=>{setModal(true)}}>
-                {t[i]} <span onClick={()=>{
+              <h4 onClick={()=>{setModal(true); setTitle(i)}}>
+                {t[i]} <span onClick={(e)=>{ e.stopPropagation();
                   let myHeart = [...heart];
                   myHeart[i] += 1;
                   setHeart(myHeart);
@@ -42,19 +45,20 @@ function App() {
       }
         
         {
-          modal == true ? <Modal/> : null
+          modal == true ? <Modal title = {title} setT = {setT} t = {t}/> : null
         }
 
   </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className='modal'>
-          <h4>제목</h4>
+          <h4>{props.t[props.title]}</h4>
           <p>날짜</p>
           <p>상세내용</p>
+          <button onClick={()=>{props.setT(['여자코트 추천', '강남 우동맛집', '파이썬독학'])}}>글수정</button>
       </div>
   )
 }
